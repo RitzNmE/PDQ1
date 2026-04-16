d4016c8880c42dd5dcab818978108a8aee15b714
RitzNmE <ritter01122006@gmail.com>
2026-04-16 07:56:37 +0700

    Tugas praktikum 4

--- KasirInimaret.java
+++ KasirInimaret.java
@@ -4,26 +4,29 @@ public class KasirInimaret {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String[] namaBarang = new String[5];
-        int[] qty = new int[5];
-        double[] harga = new double[5];
         int jumlahItem = 0;
+        double total = 0;
 
         System.out.println("KASIR INIMARE");
 
         for (int i = 0; i < 5; i++) {
             System.out.println("\nBarang ke-" + (i + 1));
+
             System.out.print("Nama barang  : ");
-            namaBarang[i] = sc.nextLine();
+            String nama = sc.nextLine();
 
             System.out.print("Harga satuan : Rp ");
-            harga[i] = sc.nextDouble();
+            double harga = sc.nextDouble();
 
             System.out.print("Jumlah (qty) : ");
-            qty[i] = sc.nextInt();
+            int qty = sc.nextInt();
             sc.nextLine();
 
+            double subtotal = harga * qty;
+            total += subtotal;
             jumlahItem++;
+
+            System.out.println("Subtotal     : Rp " + (int) subtotal);
 
             if (i < 4) {
                 System.out.print("Tambah barang lagi? (y/n): ");
@@ -34,23 +37,18 @@ public class KasirInimaret {
             }
         }
 
-        double total = 0;
-        for (int i = 0; i < jumlahItem; i++) {
-            total += harga[i] * qty[i];
-        }
-
-        System.out.println("DATA PENJUALAN");
-        for (int i = 0; i < jumlahItem; i++) {
-            System.out.println((i + 1) + ". " + namaBarang[i] +
-                " | Qty: " + qty[i] +
-                " | Subtotal: Rp " + (int)(harga[i] * qty[i]));
-        }
-
+        System.out.println("\nRINGKASAN");
+        System.out.println("Jumlah item   : " + jumlahItem + " barang");
         System.out.println("Total Belanja : Rp " + (int) total);
 
         System.out.print("Uang Bayar    : Rp ");
         double bayar = sc.nextDouble();
-        System.out.println("Kembalian     : Rp " + (int)(bayar - total));
+
+        if (bayar < total) {
+            System.out.println("Uang tidak cukup!");
+        } else {
+            System.out.println("Kembalian     : Rp " + (int)(bayar - total));
+        }
 
         sc.close();
     }
